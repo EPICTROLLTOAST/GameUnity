a3b4e33dc58cef1c6f4e0ed3e85fff7d081fb3fd
RayanAliPachisa <rayanalipachisa@gmail.com>
2023-10-17 21:09:58 +0800

    Revert "Revert "Revert "Revert "movement""""
    This reverts commit c973e2ce775af1b8ee562f41697ec884d3882ae7.

---
+++ Game/Project23S6B/Assets/Scripts/SkeletonMovementController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonMovementController : BaseEnemyMovementScript
+{
+
+    public override void controlMovement()
+    {
+        base.controlMovement();
+
+    }
+}
