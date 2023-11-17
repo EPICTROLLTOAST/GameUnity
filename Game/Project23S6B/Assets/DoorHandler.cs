af6efb749925545d5e327d8b6274c91e3a4d55b1
Syrupy22 <awesomeyy22@gmail.com>
2023-11-17 10:01:34 +0800

    Update DoorHandler.cs

--- Game/Project23S6B/Assets/DoorHandler.cs
+++ Game/Project23S6B/Assets/DoorHandler.cs
@@ -13,6 +13,7 @@ public class DoorHandler : MonoBehaviour
     public float timeTaken = 2f;
     public Vector3 currentEulerAngles;
     public bool DoorTurning = false;
+    public float toggle = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,9 +29,11 @@ public class DoorHandler : MonoBehaviour
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         float distFromPlayer = (player.transform.position - gameObject.transform.position).magnitude;
         if(DoorTurning){
-            gameObject.GetComponent<RotateAroundPivot>().angle = maxAngle * (stopWatch / timeTaken);
-
-            if(stopWatch > timeTaken) DoorTurning = false;
+            gameObject.GetComponent<RotateAroundPivot>().angle = maxAngle * (stopWatch / timeTaken) * toggle;
+            if(stopWatch > timeTaken){
+                DoorTurning = false;
+                toggle = 0f - toggle;
+            }
         }else if(distFromPlayer <= maxDistanceFromDoor && Input.GetKeyDown(KeyCode.E)){
             stopWatch = 0; 
             DoorTurning = true;
