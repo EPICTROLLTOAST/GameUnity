37cb69de53d1a2676f1cbdd0cbacd5de617c8c02
EPICTROLLTOAST <45279424+EPICTROLLTOAST@users.noreply.github.com>
2023-12-07 20:32:15 +0800

    Added capability for player to dash
    Player can now dash

--- Game/Project23S6B/Assets/Scripts/PlayerScripts/BasePlayerController.cs
+++ Game/Project23S6B/Assets/Scripts/PlayerScripts/BasePlayerController.cs
@@ -36,7 +36,39 @@ public class BasePlayerController : MonoBehaviour, IDamageable
     float flungMultiplier = 2f;
     [SerializeField]
     float debugKnockback = 5f;
+    bool isDashing = false;
+    bool prepDashing = false;
+    float dashTime = 0f;
 
+    enum dashTypes{
+        dashRelyingOnButtons,
+        clickTheDashLocation
+    }
+
+    [SerializeField]
+    dashTypes dashType = dashTypes.clickTheDashLocation;
+    [SerializeField]
+    [Range(0f, 200f)]
+    float dashRadius = 5f;
+    [SerializeField]
+    [Range(0f, 200f)]
+    float dashSpeedClick = 1f;
+    Vector2 dashLocation;
+
+    [SerializeField]
+    [Range(0f, 1.95f)]
+    float dashTimeButtons = 1f;
+    [SerializeField]
+    [Range(0f, 200f)]
+    float dashDistanceButtons = 5f;
+    [SerializeField]
+    [Range(0f, 5f)]
+    float delayBeforeStartingTDash = 0.5f;
+
+
+    float dashTimeoutCounter = 0f;
+    bool buttonsDashSpeedSet = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -48,12 +80,18 @@ public class BasePlayerController : MonoBehaviour, IDamageable
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R)){
-            new DamageEvent(0f, damageTypes.BLUNT, player, player, debugKnockback, Vector2.left);
+        if(isDashing || prepDashing){
+            //dash timeout
+            dashTimeoutCounter += Time.deltaTime;
         }
 
+        //Enter dashing mode
+        
         if(!movementLockFlag){
-            playerMovementKeyCheck();
+            if(!prepDashing && !isDashing){
+                playerMovementKeyCheck();
+            }
+
         }else{
             timeSinceMovementLock += Time.deltaTime;
             if(timeSinceMovementLock >= timeToWaitForMovementLock){
@@ -64,11 +102,15 @@ public class BasePlayerController : MonoBehaviour, IDamageable
         }
         
         playerMouseCheck();
-        
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            print("Hello!");
-            //Just call a new damage event to damage someone
+
+
+        if(Input.GetKeyDown(KeyCode.Space) && !isDashing && !prepDashing){
+            prepDashing = true;
+            dashTimeoutCounter = 0f;
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }else if(isDashing){
+            //Stop prep dashing
+
         }
 
     }
@@ -121,6 +163,91 @@ public class BasePlayerController : MonoBehaviour, IDamageable
 
     public void playerMouseCheck()
     {
+        if(prepDashing && dashTimeoutCounter > delayBeforeStartingTDash){
+            if(dashType == dashTypes.clickTheDashLocation){
+
+                if(Input.GetMouseButtonDown(0)){
+                    //The location to dash to
+                    dashLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    if((dashLocation - (Vector2)gameObject.transform.position).magnitude > dashRadius){
+                        //if dash exceeds dash radius, player will dash to the furthest point within 
+                        //dash radius in the direction of the mouse
+                        dashLocation = (dashLocation - (Vector2)gameObject.transform.position).normalized 
+                        * dashRadius + (Vector2)gameObject.transform.position;
+                    }
+                    isDashing = true;
+                    dashTimeoutCounter = 0f;
+                    prepDashing = false;
+                }
+
+            }else if(dashType == dashTypes.dashRelyingOnButtons){
+                Vector2 direction = new Vector2(0f,0f);
+                bool buttonPressed = false;
+
+                if(Input.GetKey(KeyCode.W)){
+                    direction.y += 1f;
+                    buttonPressed = true;
+                }
+                if(Input.GetKey(KeyCode.S)){
+                    direction.y -= 1f;
+                    buttonPressed = true;
+                }
+                if(Input.GetKey(KeyCode.D)){
+                    direction.x += 1f;
+                    buttonPressed = true;
+                }
+                if(Input.GetKey(KeyCode.A)){
+                    direction.x -= 1f;
+                    buttonPressed = true;
+                }
+
+                if(buttonPressed){
+                    isDashing = true;
+                    dashTimeoutCounter = 0f;
+                    prepDashing = false;
+                    dashLocation = (Vector2)gameObject.transform.position + 
+                    direction.normalized * dashDistanceButtons;
+                    buttonsDashSpeedSet = false;
+                }
+
+
+            }
+
+        }else{
+            if(isDashing){
+                if(dashType == dashTypes.clickTheDashLocation){
+
+                    if(Vector2.Distance(dashLocation, gameObject.transform.position) <= 0.2f){
+                        isDashing = false;
+                        gameObject.transform.position = dashLocation;
+                    }
+                    gameObject.GetComponent<Rigidbody2D>().velocity = 
+                    dashSpeedClick * (dashLocation - (Vector2)gameObject.transform.position).normalized;
+                    if(dashTimeoutCounter > 1f){
+                        isDashing = false;
+                        gameObject.transform.position = dashLocation;
+                    }
+
+                }else if(dashType == dashTypes.dashRelyingOnButtons){
+
+                    if(!buttonsDashSpeedSet){
+                        gameObject.GetComponent<Rigidbody2D>().velocity = 
+                        (dashLocation - (Vector2)gameObject.transform.position) / dashTimeButtons;
+
+                        buttonsDashSpeedSet = true;
+                    }
+
+                    //End the dash if enough time has passed
+                    if(dashTimeoutCounter > dashTimeButtons){
+                        isDashing = false;
+                        gameObject.transform.position = dashLocation;                        
+                    }   
+
+                }
+
+            }
+        }
+
         //mouse actions
         // if (Input.GetMouseButtonDown(0))
         // {
@@ -156,6 +283,11 @@ public class BasePlayerController : MonoBehaviour, IDamageable
 
     public void recceiveDamage(DamageEvent damageEvent)
     {
+        //Ignore damage if immune
+        if(immunityCheck(damageEvent)){
+            return;
+        }
+
         health -= damageEvent.damageAmount;
         if (health <= 0 )
         {
@@ -195,6 +327,18 @@ public class BasePlayerController : MonoBehaviour, IDamageable
             damageEvent.target.GetComponent<Rigidbody2D>().AddForce(damageEvent.getKnockbackDirection().normalized * knockbackSpeed * 10);
             
         }
+    }
+
+    public bool immunityCheck(DamageEvent damageEvent){
+        if(isDashing){
+            return true;
+        }else if(prepDashing){
+            prepDashing = false;
+
+            return false;
+        }
+
+        return false;
     }
 
     private void die()
